998f3d4bdcc2902db6264250f4658b457dd5ca01
hong <kevin0928@naver.com>
2023-01-26 22:35:59 +0900

    readMe

--- ReadMe.py
+++ ReadMe.py
@@ -1,2 +1,6 @@
 # https://velog.io/@printver_2world/django-20.-%EB%A1%9C%EA%B7%B8%EC%9D%B8-%EB%A1%9C%EA%B7%B8%EC%95%84%EC%9B%83-%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0
 # 로그인 기능 구현
+
+from PIL import Image
+image = Image.open("account.png")
+image.show()
