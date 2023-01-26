e24dd46f182a9e9dd9a51ec518814721cfe3e66e
YongJun <104314593+yongjun-hong@users.noreply.github.com>
2023-01-26 22:40:00 +0900

    Update ReadMe.py

--- ReadMe.py
+++ ReadMe.py
@@ -1,6 +1,3 @@
 # https://velog.io/@printver_2world/django-20.-%EB%A1%9C%EA%B7%B8%EC%9D%B8-%EB%A1%9C%EA%B7%B8%EC%95%84%EC%9B%83-%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0
 # 로그인 기능 구현
-
-from PIL import Image
-image = Image.open("account.png")
-image.show()
+![account](https://user-images.githubusercontent.com/104314593/214849592-e984102b-76a5-4561-b3f8-6c1629fb05af.png)
