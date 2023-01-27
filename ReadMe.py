9ab5a27758a6a4c37b6d19c332dbb315b35862a1
YongJun <104314593+yongjun-hong@users.noreply.github.com>
2023-01-27 19:46:55 +0900

    Update ReadMe.py

--- ReadMe.py
+++ ReadMe.py
@@ -1,3 +1,4 @@
 # https://velog.io/@printver_2world/django-20.-%EB%A1%9C%EA%B7%B8%EC%9D%B8-%EB%A1%9C%EA%B7%B8%EC%95%84%EC%9B%83-%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0
 # 로그인 기능 구현
 ![account](https://user-images.githubusercontent.com/104314593/214849592-e984102b-76a5-4561-b3f8-6c1629fb05af.png)
+<img width="100%" src="https://user-images.githubusercontent.com/104314593/214849592-e984102b-76a5-4561-b3f8-6c1629fb05af.png"/>
