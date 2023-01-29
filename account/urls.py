7d0e0bb83c1c825deb5d3ed3d3bf90d90642c416
hong <kevin0928@naver.com>
2023-01-29 22:48:19 +0900

    login&logout&signup

--- account/urls.py
+++ account/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = "users"
 urlpatterns = [
-    path('', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('', views.login, name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'), # 코드 추가하기
     path('signup', views.signup, name='signup')
 ]
--- account/views.py
+++ account/views.py
@@ -15,3 +15,17 @@ def signup(request):
             auth.login(request, user)
             return redirect('blog_views')
     return render(request, 'signup.html')
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            auth.login(request, user)
+            return redirect('blog_views')
+        else:
+            return render(request, 'login.html', {'error' : 'username or password is incorrect.'})
+    else:
+        return render(request, 'login.html')
